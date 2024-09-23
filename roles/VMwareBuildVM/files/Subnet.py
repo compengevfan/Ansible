6ea607ae22e81b927a423ff695d3c33accf1fa68
CompEngLSUFan <chris.dupree@gmail.com>
2024-09-23 16:32:00 -0400

    python is getting an array

--- roles/VMwareBuildVM/files/Subnet.py
+++ roles/VMwareBuildVM/files/Subnet.py
@@ -2,10 +2,10 @@ import sys
 
 subnet = sys.argv
 
-print(subnet)
+# print(subnet)
 
-# if subnet == 23:
-#     print("255.255.254.0")
+if subnet[1] == 23:
+    print("255.255.254.0")
 
-# if subnet == 24:
-#     print("255.255.255.0")
+if subnet[1] == 24:
+    print("255.255.255.0")
