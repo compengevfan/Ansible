2c04e9999f3435ff7357037db016abb3846da5f5
CompEngLSUFan <chris.dupree@gmail.com>
2024-09-23 16:29:13 -0400

    what is the variable in python?

--- roles/VMwareBuildVM/files/Subnet.py
+++ roles/VMwareBuildVM/files/Subnet.py
@@ -2,8 +2,10 @@ import sys
 
 subnet = sys.argv
 
-if subnet == 23:
-    print("255.255.254.0")
+print(subnet)
 
-if subnet == 24:
-    print("255.255.255.0")
+# if subnet == 23:
+#     print("255.255.254.0")
+
+# if subnet == 24:
+#     print("255.255.255.0")
